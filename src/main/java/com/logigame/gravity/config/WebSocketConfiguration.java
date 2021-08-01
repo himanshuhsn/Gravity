c77f6c70ef7a1b2f55e5652008590946be163823
himanshu <himanshu000666@gmail.com>
2021-08-01 22:39:31 +0530

    add cross-origin allow for prod

--- src/main/java/com/logigame/gravity/config/WebSocketConfiguration.java
+++ src/main/java/com/logigame/gravity/config/WebSocketConfiguration.java
@@ -1,5 +1,6 @@
 package com.logigame.gravity.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,9 +10,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+	
+	@Value("${spring.crossoriginallow.url}")
+	private String crossOriginAllowUrl = "null";
+	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/gameplay").setAllowedOrigins("null").withSockJS();
+		registry.addEndpoint("/gameplay").setAllowedOrigins(crossOriginAllowUrl).withSockJS();
 	}
 
 	@Override
